449a94c702cad12204486fb72f8b5b95594b4f5d
Yeasin <yeasin30112001@gmail.com>
2021-01-31 17:07:27 -0600

    pin macthed functionality added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // random pin generate
-document.getElementById('pin-generate-btn').addEventListener('click', function(){
+document.getElementById('pin-generate-btn').addEventListener('click', function () {
     const pin = document.getElementById('pin-input-field');
     const randomNumber = Math.random() * 10000 + "";
     const pinGenerate = randomNumber.split('.')[0];
@@ -8,8 +8,27 @@ document.getElementById('pin-generate-btn').addEventListener('click', function()
 
 
 //key button function
-function pressKey(key){
+function pressKey(key) {
     const typedPin = document.getElementById('pin-type-field');
     const pin = typedPin.value + key;
     typedPin.value = pin;
 }
+
+
+// check pin is matched or not
+document.getElementById('submit-btn').addEventListener('click', function () {
+    const pinCode = document.getElementById('pin-input-field');
+    const typedPinCode = document.getElementById('pin-type-field');
+    if (pinCode.value == "") {
+        alert('there is no pin! Please, click on Generate Pin first');
+        document.getElementById('matched-notify').style.display = 'none';
+    }
+    else if (pinCode.value == typedPinCode.value) {
+        document.getElementById('matched-notify').style.display = 'block';
+        document.getElementById('not-matched-notify').style.display = "none";
+    }
+    else{
+        document.getElementById('not-matched-notify').style.display = "block";
+        document.getElementById('matched-notify').style.display = 'none';
+    }
+});
